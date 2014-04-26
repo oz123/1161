0e099614aec24d8b9ee38b7a7a9ce22eb9c6b715
oz123 <nahumoz@gmail.com>
2014-04-26 09:30:11 +0200

    Add task to walk all values in the tree

--- src/Tree.java
+++ src/Tree.java
@@ -4,40 +4,67 @@
 
 public class Tree {
 
-	
 	public int value;
 	public Tree lson;
 	public Tree sybling;
-	
-	public Tree(int value){
-		this.value = value; 
-		
+
+	public Tree(int value) {
+		this.value = value;
+
 	}
-	
-	private static Tree build_tree(){
-		Tree tree = new Tree(4);
-		tree.lson = new Tree(8);
-		tree.lson.lson = new Tree(16);
-		tree.lson.lson.sybling = new Tree(5);
-		tree.lson.lson.sybling.sybling = new Tree(1); 
-		return tree;
-		
+
+	private static Tree build_tree() {
+		Tree t = new Tree(4);
+		// left branch
+		t.lson = new Tree(8);
+		t.lson.lson = new Tree(16);
+		t.lson.lson.sybling = new Tree(5);
+		t.lson.lson.sybling.sybling = new Tree(1);
+		// middle branch
+		t.lson.sybling = new Tree(12);
+		t.lson.sybling.lson = new Tree(13);
+		t.lson.sybling.lson.sybling = new Tree(9);
+		// free standing leaf
+		t.lson.sybling.sybling = new Tree(17);
+		// right most branch
+		t.lson.sybling.sybling.sybling = new Tree(3);
+		t.lson.sybling.sybling.sybling.lson = new Tree(7);
+		t.lson.sybling.sybling.sybling.lson.sybling = new Tree(11);
+
+		return t;
+
 	}
-	public void print_keys(){
-		
+    
+	/* 
+	 * @param tree - a Tree as defined 
+	 * Walk a tree (of N degree) and print all the values
+	 */
+	public static Tree walk_tree(Tree tree) {
+		System.out.println(tree.value);
+	    if (tree.lson != null){
+	    	walk_tree(tree.lson);
+	    }
+	    if (tree.sybling != null) {
+	    	walk_tree(tree.sybling);
+	    }
+	    return tree;
 	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		Tree tree = build_tree();
-		
+
 		System.out.println("Hello World!");
+		/*
 		System.out.println(tree.value);
 		System.out.println(tree.lson.value);
 		System.out.println(tree.lson.lson.value);
 		System.out.println(tree.lson.lson.sybling.value);
+		*/
+		walk_tree(tree);
 	}
 
 }
